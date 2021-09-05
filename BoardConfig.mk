fecf67220db69f48fab6a6bd3dc05b70ee27b325
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-09-05 16:06:04 +0530

    r5x: power-libperfmgr: Switch to powerhint for battery saver
     * also nuke power mode extension code cuz we dont
       need it while we have in-tree power hal
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,7 +178,6 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Recovery
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
