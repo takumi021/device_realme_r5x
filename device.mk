969221cd76a6c32f3b21c38d3d6b668e68c5d27a
TheScarastic <warabhishek@gmail.com>
2022-01-19 09:36:35 +0530

    r5x: RealmeDirac: Adapt to S style
     * Use Theme.SubSettingsBase for theme
     * Replace PreferenceActivity with CollapsingToolbarBaseActivity
     * Add Titles to prefernce screen
     * Remove onOptionsItemSelected and where neccessary move to activity
    
    Change-Id: Ie1a846ae1883a8e2c4c3603fdd56c09c92268424
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -687,3 +687,6 @@ $(call inherit-product, vendor/realme/r5x/r5x-vendor.mk)
 # Remove Unwanted Packages
 PRODUCT_PACKAGES += \
     RemovePackages
+
+# RealmeDirac
+$(call inherit-product, $(LOCAL_PATH)/RealmeDirac/dirac.mk)
