618206fec9a9c29394c6a8e737faa755aa64ebf0
Sarthak Roy <sarthakroy2002@gmail.com>
2022-07-14 09:32:30 +0000

    r5x: Move RealmeDirac into app * Rearrange some stuff

--- device.mk
+++ device.mk
@@ -417,6 +417,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
 
+# RealmeDirac
+$(call inherit-product, $(LOCAL_PATH)/app/RealmeDirac/dirac.mk)
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
@@ -689,6 +692,3 @@ PRODUCT_PACKAGES += \
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/realme/r5x/r5x-vendor.mk)
-
-# RealmeDirac
-$(call inherit-product, $(LOCAL_PATH)/RealmeDirac/dirac.mk)
