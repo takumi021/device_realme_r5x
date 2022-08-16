1bf54348c6bc13fc3f9fbedbf5253a607a0cd543
takumi021 <omrameshpatel@gmail.com>
2022-08-16 16:35:47 +0000

    r5x: Declare Board

--- device.mk
+++ device.mk
@@ -67,6 +67,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Board
+PRODUCT_BOARD_PLATFORM := trinket
+PRODUCT_USES_QCOM_HARDWARE := true
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
