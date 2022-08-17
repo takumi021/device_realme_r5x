97158ec35845978028593ad9612097896fc2280a
CrisBal <crisbaladim@yahoo.gr>
2022-08-17 05:02:34 +0000

    r5x: Silence kernel build warning Enough... HOSTCC  scripts/mod/sumversion.o clang-12: warning: argument unused during compilation: '-fuse-ld=lld' [-Wunused- command-line-argument] clang-12: warning: argument unused during compilation: '-fuse-ld=lld' [-Wunused- command-line-argument]   HOSTCC  scripts/mod/file2alias.o clang-12: warning: argument unused during compilation: '-fuse-ld=lld' [-Wunused- command-line-argument]   HOSTLD  scripts/mod/modpost

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,8 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_SOURCE := kernel/realme/r5x
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := vendor/RMX1911_defconfig
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+   HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
