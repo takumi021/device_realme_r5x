c368c61defc3ffab2606525ab887228effa0984d
adi8900 <adrianszymanski242@gmail.com>
2022-08-18 12:59:48 +0000

    r5x: sepolicy: Attach proper property_type into device_id * as per https://source.android.com/devices/architecture/configuration/add-system-properties.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,6 +203,7 @@ SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Treble
 PRODUCT_VENDOR_MOVE_ENABLED := true
