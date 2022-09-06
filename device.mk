e67c51a0b34f5644ac41e48d5a99573b3e1ba4e4
kubersharma001 <kubersharma001@gmail.com>
2022-09-06 08:59:51 +0000

    r5x: Build GcamGO from Source This reverts commit 7d38402543fd3b0b0ee6c48b4612d74b3b883d26.

--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libstdc++.vendor \
     libdng_sdk.vendor \
-    GcamGo
+    GCamGOPrebuilt
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
