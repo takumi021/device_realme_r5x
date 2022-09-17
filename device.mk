890f6f3e3813d670995fd4977cf0f4084951a52c
takumi021 <omrameshpatel@gmail.com>
2022-09-17 06:11:02 +0000

    r5x: Build thermal@1.0-impl and thermal.trinket also add manifest entry

--- device.mk
+++ device.mk
@@ -469,7 +469,9 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    android.hardware.thermal@1.0-impl \
+    thermal.trinket
 
 # USB
 PRODUCT_PACKAGES += \
