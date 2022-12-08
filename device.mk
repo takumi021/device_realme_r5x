d56bfe3acf6a2bbd76cc38e6b266385185055ccb
takumi021 <omrameshpatel@gmail.com>
2022-12-08 03:58:15 +0000

    r5x: Don't Build GcamGo

--- device.mk
+++ device.mk
@@ -106,9 +106,6 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor \
     libdng_sdk.vendor
 
-PRODUCT_PACKAGES += \   
-    GCamGOPrebuilt
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
