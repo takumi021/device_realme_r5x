900f3c64203538beb561228f7463ef0a23874b49
Nolen Johnson <johnsonnolen@gmail.com>
2022-12-31 09:11:12 +0000

    r5x: Move to libhidl shim in hardware/lineage/compat

--- device.mk
+++ device.mk
@@ -232,7 +232,8 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
