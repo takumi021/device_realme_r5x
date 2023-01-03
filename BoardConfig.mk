3ea57a507aedf84bd9cee269f244bd0fbd43d29f
Arian <arian.kulmer@web.de>
2023-01-03 15:18:15 +0000

    r5x: Switch to legacy-um qcom sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,7 +196,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SELinux
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
