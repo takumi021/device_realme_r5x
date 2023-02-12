ff4d6d03fb6aac133a5a32f8767b911de424976f
Mathieu Chartier <mathieuc@google.com>
2023-02-12 18:46:08 +0000

    r5x: Always preopt extracted APKs Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914 Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: takumi021 <omrameshpatel@gmail.com>

--- device.mk
+++ device.mk
@@ -472,6 +472,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
@@ -492,9 +496,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
-
-# Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Vibrator
 PRODUCT_PACKAGES += \
