661f6512382a12eea703a276d1b3286c4e57333b
Om Patel <omrameshpatel@gmail.com>
2023-03-18 15:31:33 +0000

    bearmonets: rootdir: Fix path for modem in baseband script
    Signed-off-by: Om Patel <omrameshpatel@gmail.com>

--- rootdir/bin/init.set_baseband.sh
+++ rootdir/bin/init.set_baseband.sh
@@ -1,13 +1,13 @@
 #!/system/bin/sh
 
 tag="init.set_baseband.sh"
-baseband=$(strings -n 60 /vendor/firmware_mnt/image/modem.b19 | grep QC_IMAGE_VERSION_STRING | cut -c 25-)
+baseband=$(strings -n 60 /firmware/image/modem.b19 | grep QC_IMAGE_VERSION_STRING | cut -c 25-)
 
 if [ ! -z "$baseband" ]; then
     setprop gsm.version.baseband $baseband
     log -t $tag -p i "Baseband set to $baseband"
 else
-    baseband=$(strings -n 30 /vendor/firmware_mnt/image/modem.b* | grep QC_IMAGE_VERSION_STRING | cut -c 25-)
+    baseband=$(strings -n 30 /firmware/image/modem.b* | grep QC_IMAGE_VERSION_STRING | cut -c 25-)
     if [ ! -z "$baseband" ]; then
         setprop gsm.version.baseband $baseband
         log -t $tag -p i "Baseband set to $baseband"
